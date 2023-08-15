c9ce51e2ef77140e02b57093cd3084f0662fb494
Mitali <mitali.harsh44@gmail.com>
2023-08-15 12:02:29 +0530

    project added

---
+++ CarRental/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.your-class').slick();
+  });
+
+  $(document).ready(function(){
+    $('.carousel').slick({
+    dots:true,
+    // centerMode: true,
+    });
+  });
